174dab95bc4c1b65167dc2cb5e81a7d5920878cb
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2026-07-27 19:50:27 +0530

    Add 1899A

---
+++ 800/1899A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        if(n%3==0){
+            cout<<"Second"<<endl;
+        }else{
+            cout<<"First"<<endl;
+        }
+    }
+    return 0;
+}
